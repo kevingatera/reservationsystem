6b83aaad890d242a3c137e72c3319e7462a836f5
kevingatera <kgate066@uottawa.ca>
2020-04-05 12:53:47 -0400

    added a skeleton property rest controller

---
+++ src/main/java/com/ehotelreservations/reservationsystem/api/propertyRestController.java
@@ -0,0 +1,10 @@
+package com.ehotelreservations.reservationsystem.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/property/")
+public class propertyRestController {
+  
+}
