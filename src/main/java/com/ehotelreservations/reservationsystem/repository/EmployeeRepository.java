c73dcbe94f94afcf45cb752705c2108a9a1cecd8
kevingatera <kgate066@uottawa.ca>
2020-04-05 12:47:07 -0400

    code cleanup in employee

--- src/main/java/com/ehotelreservations/reservationsystem/repository/EmployeeRepository.java
+++ src/main/java/com/ehotelreservations/reservationsystem/repository/EmployeeRepository.java
@@ -1,95 +1,31 @@
 package com.ehotelreservations.reservationsystem.repository;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-import com.ehotelreservations.reservationsystem.mappers.NestedRowMapper;
+// import java.time.LocalDateTime;
+
+// import com.ehotelreservations.reservationsystem.mappers.EmployeeRowMapper;
 import com.ehotelreservations.reservationsystem.model.Employee;
 
-@Repository
-public class EmployeeRepository {
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
-  @Autowired
-  private JdbcTemplate jdbcTemplate;
+  // @Query(value = "SELECT TOP 1 * FROM users u WHERE username = ':username'",
+  // nativeQuery = true)
+  // Employee findByName(@Param("name") String name);
 
-  public Employee findByEmployeeId(Long id) {
+  List<Employee> findByFirstName(String firstname);
 
-    String sql = "SELECT * FROM EMPLOYEE WHERE ID = ?";
+  List<Employee> findByLastName(String lastname);
 
-    return jdbcTemplate.queryForObject(sql, new Object[] { id }, new NestedRowMapper<>(Employee.class));
-  }
+  Employee save(Employee employee);
 
-  public List<Employee> findAll() {
-
-    String sql = "SELECT * FROM EMPLOYEE";
-
-    List<Employee> employees = jdbcTemplate.query(sql, new NestedRowMapper<>(Employee.class));
-
-    return employees;
-  }
-
-  public String findEmployeeNameById(Long id) {
-
-    String sql = "SELECT NAME FROM EMPLOYEE WHERE ID = ?";
-
-    return jdbcTemplate.queryForObject(sql, new Object[] { id }, String.class);
-
-  }
-
-  public int count() {
-
-    String sql = "SELECT COUNT(*) FROM EMPLOYEE";
-
-    // queryForInt() is Deprecated
-    // https://www.mkyong.com/spring/jdbctemplate-queryforint-is-deprecated/
-    // int total = jdbcTemplate.queryForInt(sql);
-
-    return jdbcTemplate.queryForObject(sql, Integer.class);
-
-  }
-
-  public List<Employee> findByFirstName(String firstname) {
-    return null;
-  }
-
-  public Employee findById(long id) {
-    return null;
-  }
-
-  public void save(Employee employee) {
-    String sql = "insert into employee(first_name,last_name,email,position,phone,salary,branch_ID) "
-        + "values(?,?,?,?,?,?,?,?)";
-
-    Object[] params = new Object[] { employee.getFirst_name(), employee.getLast_name(), employee.getEmail(),
-        employee.getPosition(), employee.getPhone(), employee.getSalary(), employee.getBranch_ID()
-
-        // TODO:
-    };
-    // user.getName(), user.getPrice() };
-
-    jdbcTemplate.update(sql, params);
-  }
-
-  // public int save(Employee employee) {
-  // return jdbcTemplate.update("insert into employee (name, age, created_date)
-  // values(?,?,?)", employee.getName(),
-  // employee.getAge(), LocalDateTime.now());
-  // }
-
-  // @GetMapping("/bulkcreate")
-  // public String bulkcreate(){
-  // // save a single Customer
-  // repository.save(new Customer("Rajesh", "Bhojwani"));
-
-  // // save a list of Customers
-  // repository.saveAll(Arrays.asList(new Customer("Salim", "Khan")
-  // , new Customer("Rajesh", "Parihar")
-  // , new Customer("Rahul", "Dravid")
-  // , new Customer("Dharmendra", "Bhojwani")));
-
-  // return "Customers are created";
-  // }
+  // @Query(value = "INSERT INTO users (id, username, password, created_date)
+  // values(':id', ':username', ':password', ':created_date')", nativeQuery =
+  // true)
+  // void save(@Param("id") String id, @Param("username") String username,
+  // @Param("password") String password,
+  // @Param("created_date") LocalDateTime created_date);
 }
