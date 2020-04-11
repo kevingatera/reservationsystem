759b2a5855daddaaec86897ff210814059794ddf
kevingatera <kgate066@uottawa.ca>
2020-04-11 02:49:06 -0400

    change id from string to int

--- src/main/java/com/ehotelreservations/reservationsystem/model/address/Guest_Address.java
+++ src/main/java/com/ehotelreservations/reservationsystem/model/address/Guest_Address.java
@@ -3,10 +3,12 @@ package com.ehotelreservations.reservationsystem.model.address;
 import com.ehotelreservations.reservationsystem.model.Address;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
+@EqualsAndHashCode(callSuper = false)
 public class Guest_Address extends Address {
 
-  String guestID;
+  int guestID;
 
 }
